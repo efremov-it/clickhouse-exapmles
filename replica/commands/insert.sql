88781c67ade78b7ec5912c5a389f62881c5d1efb
Александр Выскребцев <efremov.ava.it@gmail.com>
2024-10-17 12:12:30 +0300

    replica: added keeper settings, replica+keeper and simple documentation

---
+++ replica/commands/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO company_db.events_distr
+SELECT
+    now(),
+    rand(1),                               -- Random unique identifier
+    if(rand() % 2 = 0, 'view', 'contact')   -- Random event type: either 'view' or 'contact'
+FROM numbers(3);                          -- Number of rows to generate (10 in this case)
+
+SELECT * FROM company_db.events_distr;
--- replica/commands/setup.sql
+++ replica/commands/setup.sql
@@ -11,10 +11,3 @@ ORDER BY (uid);
 
 CREATE TABLE IF NOT EXISTS company_db.events_distr ON CLUSTER '{cluster}' AS company_db.events
 ENGINE = Distributed('{cluster}', company_db, events, uid);
-
-INSERT INTO company_db.events_distr
-SELECT
-    now() - INTERVAL rand() % 1000 SECOND, -- Random timestamp within the last 1000 seconds
-    rand(1),                               -- Random unique identifier
-    if(rand() % 2 = 0, 'view', 'contact')   -- Random event type: either 'view' or 'contact'
-FROM numbers(3);                          -- Number of rows to generate (10 in this case)
